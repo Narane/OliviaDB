295e6f47e6a579312801bac050b388ebe30f9330
Narane <h86kim@uwaterloo.ca>
2014-11-20 19:52:22 -0500

    login reads from DB

--- src/java/ece356/MenuServlet.java
+++ src/java/ece356/MenuServlet.java
@@ -4,11 +4,11 @@ import java.util.Date;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import UserTypes.*;
+import java.sql.SQLException;
 
 public class MenuServlet extends HttpServlet {
-     
     public void doPost(HttpServletRequest req, HttpServletResponse res)
-        throws ServletException, IOException {
+        throws ServletException,  IOException {
 
         HttpSession session = req.getSession();
         Boolean loggedIn = (Boolean) session.getAttribute("loggedIn");
@@ -16,11 +16,25 @@ public class MenuServlet extends HttpServlet {
         String password = (String) session.getAttribute("password");
         Date lastVisit = (Date) session.getAttribute("lastVisit");;
         
-        int userTestIndex = 0;
-        if(username != null && username.contains("doctor")){
-            userTestIndex = 1;
+        //Example functionality to get user back
+        //Need to catch the errors that come from a called function...
+        //If we don't have try/catch here, we're gonna need to put the catch
+        // errors from the function but that's going to override return type
+        // of this function; that is NOT POSSIBLE
+        String role = "";
+        try{
+            role = UserDBAO.getRole(username);
+        } catch(Exception e){
+            req.setAttribute("exception", e);
+            // Set the name of jsp to be displayed if connection fails
+            String url = "/error.jsp";
+            getServletContext().getRequestDispatcher(url).forward(req, res);
         }
-        String testText = UserFinder.GetUser(userTestIndex).GetTestValue();
+        
+        //Example on calling a specific user function based on role passed in.
+        // UserFinder returns a particular user from UserTypes, then any
+        //  functions can be called upon that returned user type
+        String testText = UserFinder.GetUser(role).GetTestValue();
         
         if (loggedIn == null) {
             loggedIn = new Boolean(false);
@@ -39,13 +53,13 @@ public class MenuServlet extends HttpServlet {
             out.println("<head><title>Main Menu</title></head>");
 
             out.println("<p>Welcome back, " + username);
+            out.println("<p>You belong to group: " + role);
             out.println("<p>You last visited on " + lastVisit);
             lastVisit = new Date();
             session.setAttribute("lastVisit", lastVisit);
             
             ///////////////////////////whoa
-            out.println("");
-            out.println("You seem to belong to: " + testText);
+            out.println("<p>Your message of the day: " + testText);
         }
         else
         {
