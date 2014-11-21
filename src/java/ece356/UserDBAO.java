82f2b294b90d5069174933773ac8a5a94e13e144
Narane <h86kim@uwaterloo.ca>
2014-11-20 19:55:03 -0500

    login reads from DB

--- src/java/ece356/UserDBAO.java
+++ src/java/ece356/UserDBAO.java
@@ -8,13 +8,26 @@ public class UserDBAO {
     public static final String user = "user_h86kim";
     public static final String pwd = "user_h86kim";
     
-    public static void testConnection()
+    public static String getRole(String username)
         throws ClassNotFoundException, SQLException {
         Statement stmt;
         Connection con;
         Class.forName("com.mysql.jdbc.Driver");
         con = DriverManager.getConnection(url, user, pwd);
         stmt = con.createStatement();
-        con.close();
+        
+        String query = "select role " +
+                   "from ece356_22_2014.User " +
+                   "where Username = \"" + username + "\"";
+        
+        ResultSet rs = stmt.executeQuery(query);
+        String returnval = "";
+        while (rs.next()) {
+            returnval = (String)rs.getString("role");
+            con.close();
+            return returnval;
+        }
+        return "";
     }
+   
 }
