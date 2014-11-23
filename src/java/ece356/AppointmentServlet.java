efa85bd2a876d23cdd0cf58eea48b11260fcf48b
Narane <h86kim@uwaterloo.ca>
2014-11-23 03:19:19 -0500

    Setup Appointments and Finance stuff with templates

--- src/java/ece356/AppointmentServlet.java
+++ src/java/ece356/AppointmentServlet.java
@@ -1,6 +1,5 @@
 package ece356;
 import java.io.*;
-import java.util.Date;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
@@ -8,27 +7,29 @@ import javax.servlet.http.*;
  *
  * @author aarchen
  */
-public class AppointmentServlet extends HttpServlet {
+public class AppointmentServlet extends SecureHTTPServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest req, HttpServletResponse res)
-            throws ServletException, IOException {
+    public String pageTitle(){
+        return "View Appointments";
+    }
+    
+    @Override
+    public void innerFunction(
+            HttpServletRequest req, 
+            HttpServletResponse res, 
+            ServletOutputStream out)
+        throws ServletException,  IOException {
         
         HttpSession session = req.getSession();
         Boolean loggedIn = (Boolean) session.getAttribute("loggedIn");
         String username = (String) session.getAttribute("username");
-        
         String role = "";
         try{
             role = UserDBAO.getRole(username);
+            if(!(role.equals("patient") || role.equals("doctor") || role.equals("staff") || role.equals("superuser"))){
+                session.invalidate();
+                res.sendRedirect("LoginServlet");
+            }
         } catch(Exception e){
             req.setAttribute("exception", e);
             // TODO: Change error functionality to do something other than display error
@@ -36,85 +37,24 @@ public class AppointmentServlet extends HttpServlet {
             getServletContext().getRequestDispatcher(url).forward(req, res);
         }
         
-        if(loggedIn == null){
-            loggedIn = new Boolean(false);
-        }
-        
-        if(loggedIn == false){
-            res.sendRedirect("LoginServlet");
-        }
-        
-        res.setContentType("text/html");
-        ServletOutputStream out = res.getOutputStream();
-        out.println(MarkupHelper.HeadOpen("Main Menu", role));
-        out.println("<html>");
-        out.println("<body>");
-       
-        if(loggedIn.booleanValue()==true && role == "patient"){
+        StringBuilder sb = new StringBuilder(128);
+        if(role.equals("patient")){
             //View past and current appointments
             //Update personal info
-            out.println("Check your appointments, " + username);
-            
+            sb.append("Check your appointments, " + username);
         }
-        
-        else if(loggedIn.booleanValue()==true && role == "doctor"){
+        else if(role.equals("doctor")){
             //View current appointments
-            out.println("Check your appointments, Doctor " + username);
+            sb.append("Check your appointments, Doctor " + username);
             
         }
-        
-        else if(loggedIn.booleanValue()==true && role == "staff"){
+        else if(role.equals("staff")){
             //Enter, delete, update appointments
             //Update patient info
-            out.println("Check your appointments, " + username);
-            
+            sb.append("Check your appointments, " + username);
         }
+        out.println(sb.toString());
         
-        else{
-            /* Get out of here! */
-        }
-        
-        out.println(MarkupHelper.HeadClose());
-        
+        //do stuff from here on out...
     }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
 }
--- src/java/ece356/FinanceServlet.java
+++ src/java/ece356/FinanceServlet.java
@@ -1,86 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ece356;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author bleskows
- */
-public class FinanceServlet extends HttpServlet {
+public class FinanceServlet extends SecureHTTPServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet FinanceServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet FinanceServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+    public String pageTitle(){
+        return "Finance Hub";
     }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
+    public void innerFunction(
+            HttpServletRequest req, 
+            HttpServletResponse res, 
+            ServletOutputStream out)
+        throws ServletException,  IOException {
+        
     }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
 }
