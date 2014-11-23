8863aa0ce566476908f5556809439be52dfcf8d9
ZhongBot <zhong.botian@gmail.com>
2014-11-22 19:31:59 -0500

    Added class for ResultSet and List return data

---
+++ src/java/ece356/ResultData.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ece356;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Andy
+ */
+public class ResultData {
+    ResultSet rs;
+    ArrayList l;
+}
