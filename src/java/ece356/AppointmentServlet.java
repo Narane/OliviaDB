d095cb20a57b54bcecae6d3aeebdf6637e4ea24a
Aaron Chen <a49chen@uwaterloo.ca>
2014-11-24 03:25:51 -0500

    Added patient appointment viewing

--- src/java/ece356/AppointmentServlet.java
+++ src/java/ece356/AppointmentServlet.java
@@ -1,4 +1,5 @@
 package ece356;
+import static ece356.UserDBAO.schema;
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
@@ -8,7 +9,7 @@ import javax.servlet.http.*;
  * @author aarchen
  */
 public class AppointmentServlet extends SecureHTTPServlet {
-
+    
     public String pageTitle(){
         return "View Appointments";
     }
@@ -38,22 +39,68 @@ public class AppointmentServlet extends SecureHTTPServlet {
         }
         
         StringBuilder sb = new StringBuilder(128);
-        if(role.equals("patient")){
-            //View past and current appointments
-            //Update personal info
-            sb.append("Check your appointments, " + username);
+        try{
+            if(role.equals("patient")){
+
+                //Display most current appointment
+                String currentAppointmentQuery = "select LastName as \"Doctor Name\", min(StartTime) as \"Appointment Time (yyyy-mm-dd hh:mm:ss)\" from " +
+                        "(" + schema + ".appointment natural join " + schema + ".user)"+
+                        "where PatientUsername = \"" + username + "\" and DoctorUsername = Username and StartTime > SYSDATE()";
+
+                QueryResult currentAppointment = UserDBAO.executeQuery(currentAppointmentQuery);
+                if(currentAppointment.getResultSet().size() <= 0){
+                    sb.append("<br><h3>You currently have no upcoming appointments</h3></br>");
+                }
+                else{
+                    sb.append("<br><h3>Your Upcoming Appointment:</h3></br>" + UserDBAO.generateTable(currentAppointment));
+                }
+
+                //Display future appointments
+                String futureAppointmentsQuery = "select LastName as \"Doctor Name\", StartTime as \"Appointment Time (yyyy-mm-dd hh:mm:ss)\" from " +
+                        "(" + schema + ".appointment natural join " + schema + ".user)"+
+                        "where PatientUsername = \"" + username + "\" and DoctorUsername = Username and StartTime > SYSDATE()" +
+                        "order by StartTime desc";
+
+                QueryResult futureAppointments = UserDBAO.executeQuery(futureAppointmentsQuery);
+                if(futureAppointments.getResultSet().size() <= 0){
+                    sb.append("<br><h3>You currently have no future appointments</h3></br>");
+                }
+                else{
+                    sb.append("<br><h3>Your Future Appointments:</h3></br>" + UserDBAO.generateTable(futureAppointments));
+                }
+
+                //Display past appointments            
+                String pastAppointmentsQuery = "select LastName as \"Doctor Name\", StartTime as \"Appointment Time (yyyy-mm-dd hh:mm:ss)\" from " +
+                        "(" + schema + ".appointment natural join " + schema + ".user)"+
+                        "where PatientUsername = \"" + username + "\" and DoctorUsername = Username and StartTime < SYSDATE()" +
+                        "order by StartTime desc";
+
+                QueryResult pastAppointments = UserDBAO.executeQuery(pastAppointmentsQuery);
+                if(pastAppointments.getResultSet().size() <= 0){
+                    sb.append("<br><h3>You currently have no past appointments</h3></br>");
+                }
+                else{
+                    sb.append("<br><h3>Your Past Appointments:</h3></br>" + UserDBAO.generateTable(pastAppointments));
+                }
+            }
+            else if(role.equals("doctor")){
+                //View current appointments
+                sb.append("Check your appointments, Doctor " + username);
+
+            }
+            else if(role.equals("staff")){
+                //Enter, delete, update appointments
+                //Update patient info
+                sb.append("Check your appointments, " + username);
+            }
+            out.println(sb.toString());
         }
-        else if(role.equals("doctor")){
-            //View current appointments
-            sb.append("Check your appointments, Doctor " + username);
-            
+        catch(Exception e){
+            req.setAttribute("exception", e);
+            // TODO: Change error functionality to do something other than display error
+            String url = "/error.jsp";
+            getServletContext().getRequestDispatcher(url).forward(req, res);
         }
-        else if(role.equals("staff")){
-            //Enter, delete, update appointments
-            //Update patient info
-            sb.append("Check your appointments, " + username);
-        }
-        out.println(sb.toString());
         
         //do stuff from here on out...
     }
