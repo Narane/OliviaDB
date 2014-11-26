f8841abbb9dbfba383e2d42478624f0b1b652526
Peter Yin <peter.everythingelse@gmail.com>
2014-11-26 00:04:24 -0500

    Adding metro helper functions.

---
+++ src/java/metro/Metro.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metro;
+
+/**
+ *
+ * @author Peter
+ */
+public class Metro {
+    
+    public static String inputBoxes(String name, String type, String value, String placeholder, String dataState) {
+        
+        String html = 
+                "<div class='input-control " + type + "'>\n" +
+                    "<input name='" + name + "' type='" + type + "' value='" + value + "' placeholder='" + placeholder + "' data-state='" + dataState + "'/>\n" + 
+                "</div>\n";
+        return html;
+    }
+    
+    public static String label(String string) {
+        
+        String html = 
+                "<label>" + string + "</label>";
+        
+        return html;
+    }
+    
+    public static String submitButton() {
+        
+        String html = 
+                "<input name='submit' type='submit' value='submit'>";
+        
+        return html;
+    }
+    
+    public static String textArea(String name, String value) {
+        String html =   "<div class='input-control textarea' data-role='input-control'>\n" + 
+                            "<textarea name='" + name + "'>" + value + "</textarea>\n" +
+                        "</div>\n";
+        
+        return html;
+
+    } 
+                                      
+    
+    
+}
